fbd45b95cca66829bce5634364be871ae37d7de5
Shahul Alam <alam.shahul@gmail.com>
2017-10-04 19:50:19 -0400

    Added port variable to index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var http = require('http');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
