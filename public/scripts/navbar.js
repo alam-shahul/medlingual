2bc40d3cda777f6728f064c8b8ade4b8645a1098
Shahul Alam <alam.shahul@gmail.com>
2017-10-11 18:16:29 -0400

    Added about, who we are, and interest form sections.

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   $('.nav-link.1').click(function() {
-    $('html, body').animate({scrollTop: $('#about').offset().top - 80}, 1000);
+    $('html, body').animate({
+      scrollTop: $('#about').offset().top - $('html, body').offset().top + $('html, body').scrollTop() - $('.navbar').height()
+    }, 1000);
   });
   $('.nav-link.2').click(function() {
-    $('html, body').animate({scrollTop: $('#board').offset().top - 80}, 1000);
+    $('html, body').animate({
+      scrollTop: $('#board').offset().top - $('html, body').offset().top + $('html, body').scrollTop() - $('.navbar').height()
+    }, 1000);
   });
   $('.nav-link.3').click(function() {
-    $('html, body').animate({scrollTop: $('#interest-form').offset().top -80}, 1000);
+    $('html, body').animate({
+      scrollTop: $('#interest-form').offset().top - $('html, body').offset().top + $('html, body').scrollTop() - $('.navbar').height()
+    }, 1000);
   });
 
 })
